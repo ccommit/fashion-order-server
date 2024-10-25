184924a5a2efc8467ca39ba43075b41f4ba769de
이소희 <leesohee1129@gmail.com>
2024-10-25 21:48:47 +0900

    git init

---
+++ src/main/java/com/fashion_order_server/FashionOrderServerApplication.java
@@ -0,0 +1,13 @@
+package com.fashion_order_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionOrderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionOrderServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fashion_order_server/FashionOrderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashion_order_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionOrderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
